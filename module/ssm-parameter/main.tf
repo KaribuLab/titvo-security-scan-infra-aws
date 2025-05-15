aad9ed931d8ce79eff7f0966bbac6a0df6fcae85
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-15 14:10:32 -0400

    fix: Agrega la opción de sobrescribir parámetros en la configuración de SSM para permitir actualizaciones sin errores

--- module/ssm-parameter/main.tf
+++ module/ssm-parameter/main.tf
@@ -1,7 +1,8 @@
 resource "aws_ssm_parameter" "parameters" {
-  count    = length(var.parameters)
-  name     = "${var.base_path}/${var.parameters[count.index].name}"
-  type     = var.parameters[count.index].type
-  value    = var.parameters[count.index].value
-  tags     = var.common_tags
+  count     = length(var.parameters)
+  name      = "${var.base_path}/${var.parameters[count.index].name}"
+  type      = var.parameters[count.index].type
+  value     = var.parameters[count.index].value
+  overwrite = true
+  tags      = var.common_tags
 }
